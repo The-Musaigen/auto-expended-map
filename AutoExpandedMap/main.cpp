0be558947e679e8a3af463090f4cc197afc819d0
The-Musaigen <fedya.rezan@gmail.com>
2022-03-17 00:03:18 +0200

    add files via upload

---
+++ AutoExpandedMap/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <main.h>
+
+hook_smart_t<switch_to_new_screen_t> hook_switch_to_new_screen;
+
+void __fastcall OnSwitchToNewScreen(void* manager, void* edx86, char page) {
+    if (page == kMapPage) {
+        // Read the minimal zoom value
+        // Allowes to use any other map plugins/scripts, that removes the limit
+        float minimal_zoom = memwrapper::read_memory<float>(kMovMinimalZoom);
+
+        // Installing that value
+        memwrapper::write_memory<float>(kMapZoom, minimal_zoom);
+    }
+
+    return hook_switch_to_new_screen->call(manager, page);
+}
+
+BOOL APIENTRY DllMain(_In_ HMODULE hModule, _In_ DWORD dwReasonForCall,
+                      _In_ LPVOID lpvReserved) {
+
+    if (dwReasonForCall == DLL_PROCESS_ATTACH) {
+        using std::make_unique;
+
+        // Don't interrupt us about thread events
+        DisableThreadLibraryCalls(hModule);
+
+        // Installing hook
+        hook_switch_to_new_screen = make_unique<hook_t<switch_to_new_screen_t>>(
+            kSwitchToNewScreen, OnSwitchToNewScreen);
+        hook_switch_to_new_screen->install();
+    }
+
+    return TRUE;
+}
---
+++ AutoExpandedMap/main.h
@@ -0,0 +1,25 @@
+﻿#ifndef MAIN_H_
+#define MAIN_H_
+
+// winsdk
+#include <process.h>
+
+// local
+#include "memwrapper/memwrapper.h"
+
+namespace {
+constexpr auto kSwitchToNewScreen = 0x577244;
+constexpr auto kMapZoom           = 0xBA67AC;
+constexpr auto kMovMinimalZoom    = 0x5779FA;
+constexpr char kMapPage           = '\005';
+
+using switch_to_new_screen_t = void(__thiscall*)(void*, char);
+
+template<typename T>
+using hook_t = memwrapper::memhook<T>;
+
+template<typename T>
+using hook_smart_t = std::unique_ptr<hook_t<T>>;
+}   // namespace
+
+#endif   // !MAIN_H_
